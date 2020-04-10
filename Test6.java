be4591050225645f9a48ba67f0ee18e31c5005a8
Pravin <pravinyash@gmail.com>
2020-04-10 10:09:38 +0530

    test6

---
+++ Test6.java
@@ -0,0 +1,12 @@
+class Test6
+{
+	public static void main(String args[])
+	{
+		System.out.print("*********************************************\n");
+		System.out.print("*********************************************\n");
+		System.out.print("Test2------Hello World!");
+		System.out.print("*********************************************\n");
+		System.out.print("*********************************************\n");
+	}
+
+}
